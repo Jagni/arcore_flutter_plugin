31e1eb755f6dfb1c8bc198b368fa76816b1ffa07
IagoAlves777 <jagnidasa@gmail.com>
2020-03-12 20:56:30 -0300

    ArCoreImage treating null maps

--- lib/src/arcore_camera_image.dart
+++ lib/src/arcore_camera_image.dart
@@ -7,9 +7,11 @@ class ArCoreCameraImage {
   int height;
 
   ArCoreCameraImage.fromMap(Map<dynamic, dynamic> map) {
+    if (map != null) {
     this.jpgImage = map['jpgImage'];
     this.yuvImage = map['yuvImage'];
     this.width = map['width'];
     this.height = map['height'];
+    }
   }
 }
